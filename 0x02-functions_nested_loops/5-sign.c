692d805728a6064203c57daf552b37bc8126cf1f
Megbuzie Prosper <prospermegbuzie1@gmail.com>
2023-07-18 05:27:51 -0700

    ggdfdd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,26 @@
-#include <stdio.h>
-int main() {
-int num = 10;
-if (num > 0)
-printf("Positive\n");
-else
-printf("Non-positive\n");
-return 0;
-}
+#include "main.h"
+/**
+ * print_sign - prints the sign of a numver
+ *
+ * @n: the int to check
+ *
+ * Return: 1 and prints + if n is greater than zero
+ *
+ * 0 and prints 0 if n is zero
+ *
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+
