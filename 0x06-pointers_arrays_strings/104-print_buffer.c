965720b22d9b25ede4661525ba74231194aeb554
Megbuzie Prosper <prospermegbuzie1@gmail.com>
2023-07-26 23:29:03 -0700

    -cap_string.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,37 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int o, j, i;
+
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
