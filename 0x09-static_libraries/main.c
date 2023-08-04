a67ac5f1b31046ce98491ccc71165630336b965b
Megbuzie Prosper <prospermegbuzie1@gmail.com>
2023-08-04 07:07:20 -0700

    Updated

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,8 @@
+julien@ubuntu:~/0x09. Static Librairies$ cat main.c 
+#include "main.h"
+
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
